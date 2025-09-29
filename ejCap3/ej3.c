80b4166a5aa74b091c2788796cfe007fefd7e5c9
jflma <145713315+jflma@users.noreply.github.com>
2025-09-29 13:28:34 -0500

    Resolucion ejercicio 3 cap 3

---
+++ ejCap3/ej3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <mpi.h>
+
+int main(int argc, char* argv[]) {
+    int my_rank, comm_sz;
+    int local_val, sum;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+
+    local_val = my_rank;
+    sum = local_val;
+
+    int partner, step = 1;
+    while (step < comm_sz) {
+        if (my_rank % (2 * step) == 0) {
+            partner = my_rank + step;
+            int recv_val;
+            MPI_Recv(&recv_val, 1, MPI_INT, partner, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            sum += recv_val;
+        } else {
+            partner = my_rank - step;
+            MPI_Send(&sum, 1, MPI_INT, partner, 0, MPI_COMM_WORLD);
+            break; 
+        }
+        step *= 2;
+    }
+
+    if (my_rank == 0) {
+        printf("Global sum = %d\n", sum);
+    }
+
+    MPI_Finalize();
+    return 0;
+}
